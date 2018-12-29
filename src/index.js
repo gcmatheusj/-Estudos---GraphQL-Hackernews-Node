c1b4f1575fc2050985adffb6d3ba93eb39792798
gcmatheusj <goca1377@gmail.com>
2018-12-29 11:47:26 -0300

    cliente prisma

--- src/index.js
+++ src/index.js
@@ -1,33 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
+const { prisma } = require('./generated/prisma-client')
 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackerness Clone`,
-        feed: () => links,
+        feed: (root, args, context, info) => {
+            return context.prisma.links()
+        },
     },
     Mutation: {
-        post: (root, args) => {
-            const link = {
-                id: `link-${idCount++}`,
+        post: (root, args, context, info) => {
+            return context.prisma.createLink({
+                url: args.url,
                 description: args.description,
-                url: args.url
-            }
-            links.push(link)
-            return link
+            })
         }
     }
 }
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: { prisma }
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
